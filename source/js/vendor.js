a600038555899155056ce09da7b2a600812d9972
zhanatkim <zhanatkim@gmail.com>
2023-07-13 02:20:33 +0600

    Add promo and complete coaches
    Add promo and complete coaches

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,13 +1,23 @@
 // Swiper 7.4.1
-import Swiper from './vendor/swiper';
-import './vendor/focus-visible-polyfill';
 
-// import {Navigation, Pagination} from 'swiper/modules';
+import './vendor/focus-visible-polyfill';
+import Swiper from './vendor/swiper';
+
 
 const swiper = new Swiper('.swiper', {
   loop: true,
-  slidesPerView: 4,
-  spaceBetween: 40,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
